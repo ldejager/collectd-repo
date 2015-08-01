3ba0c9701163fc5918d8ad694647b062d7eb7e87
ldejager <leondejager@gmail.com>
2015-08-01 10:46:23 +0100

    added collectdrepo class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,9 @@
-file { '/etc/yum.repos.d/collectd.repo':
-  ensure => file,
-  owner  => 0,
-  group  => 0,
-  mode   => '0644',
-  source => 'puppet:///modules/collectdrepo/collectd.repo',
+class collectdrepo {
+  file { '/etc/yum.repos.d/collectd.repo':
+    ensure => file,
+    owner  => 0,
+    group  => 0,
+    mode   => '0644',
+    source => 'puppet:///modules/collectdrepo/collectd.repo',
+  }  
 }
